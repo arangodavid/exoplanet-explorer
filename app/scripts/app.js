6bf691b5d10bd96c5fa106ea1cb26a8489731fee
David Arango <arango.david8@gmail.com>
2019-07-11 22:50:06 -0400

    Wrapped XHR in a Promise

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,22 +36,33 @@ Instructions:
     /*
     This code needs to get wrapped in a Promise!
      */
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
+    return new Promise((resolve, reject) => {
+
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+      req.onload = function() {
+        if (req.status === 200) {
+          // It worked!
+          // You'll want to resolve with the data from req.response
+          let response = req.response;
+          resolve(response);
+          console.log(req);
+        } else {
+          // It failed :(
+          // Be nice and reject with req.statusText
+          let failedResponse = req.statusText;
+          reject(failedResponse);
+        }
+      };
+      req.onerror = function() {
+        console.log(req);
         // It failed :(
-        // Be nice and reject with req.statusText
-      }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+        // Pass a 'Network Error' to reject
+        let networkError = 'Network Error';
+        reject(networkError);
+      };
+      req.send();
+    });
   }
 
   window.addEventListener('WebComponentsReady', function() {
@@ -61,6 +72,13 @@ Instructions:
     You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
     pass 'unknown' to addSearchHeader if it rejects.
      */
-    // get('../data/earth-like-results.json')
+    get('../data/earth-lke-results.json')
+    .then(response => {
+      addSearchHeader(response);
+    })
+    .catch(error => {
+      console.log(error);
+      addSearchHeader('Unknown');
+    });
   });
 })(document);
